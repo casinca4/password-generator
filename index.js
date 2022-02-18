6c06e8d88fccd09f32b372633da4062778a58044
casinca4 <casinca@vivaldi.net>
2022-02-18 16:06:39 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const characters = Array.from('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_-+=');
+let randomPassword = [];
+const generateBtn = document.querySelector('.generate-btn');
+const passwordContainer = document.querySelectorAll('.password');
+const passwordLengthNumber = document.querySelector('#password-length-number');
+
+const generatePassword = () => {
+	passwordContainer.textContent = '';
+	for (let i = 0; i < passwordContainer.length; i++) {
+		randomPassword = [];
+		if (passwordLengthNumber.value == 'length') {
+			alert('Choose password length.');
+			break;
+		} else {
+			for (let j = 0; j < passwordLengthNumber.value; j++) {
+				let randomIndex = Math.ceil(Math.random() * characters.length - 1);
+				randomPassword.push(characters[randomIndex]);
+			}
+			const password = randomPassword.join("");
+			passwordContainer[i].textContent = password;
+			
+			const copyIcon = document.createElement("img");
+			copyIcon.src = "./copy-icon.png";
+			copyIcon.classList.add("copy-icon");
+			passwordContainer[i].append(copyIcon);
+			
+			copyIcon.addEventListener("mouseover", function() {
+				const copyPassword = document.createElement("span");
+				copyIcon.appendChild(copyPassword)
+				copyPassword.innerHTML = "Copy password";
+				console.log(copyPassword)
+				copyPassword.className = "show-copy-text"
+			})
+
+			copyIcon.addEventListener("click", function() {
+				navigator.clipboard.writeText(password);
+				alert("Copied the password: " + password)
+			})
+		}
+	}
+};
+
+generateBtn.addEventListener('click', generatePassword);
+
+
+
+
+
+
+
+
